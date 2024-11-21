c8f06b00f02138b77cc41d94f9b2b92aa8dd6203
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-21 13:41:24 +0200

    Add project files.

---
+++ LecturePortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LecturePortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
