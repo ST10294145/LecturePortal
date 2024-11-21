6bf954f15a93dfc1f141242fc940bf5e6ea7639c
st10294145@vcconnect.edu.za <ST10294145@vcconnect.edu.za>
2024-11-21 14:41:58 +0200

    Updated Program file for injection of the tables.

---
+++ LecturePortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LecturePortal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LecturePortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<LeadEntity> leadEntities { get; set; }
+    }
+}
--- LecturePortal/Program.cs
+++ LecturePortal/Program.cs
@@ -1,9 +1,15 @@
+using LecturePortal.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("LecturePortalCnn")));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
